dfc260b7539f1028c3b955b0be199e7ceda8a7a5
Dada <dadalapathy756@gmail.com>
2026-07-09 16:50:13 +0530

    Update unblock_1.py

--- unblock_1.py
+++ unblock_1.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 # --- CONFIGURATION ---
 #DATES = ["20260717", "20260718", "20260719"]
-DATES = ["20260717", "20260718", "20260719"]
+DATES = ["20260717"]
 VENUE_CODE = "ALUC"
 EVENT_CODE = "ET00504055"
 STATE_FILE = "first_day_state.json"
